a6cacf9192abed8471eb0ddd30ee543183585251
Imad Ahmad <imadahmad97@yahoo.ca>
2023-11-22 11:23:29 -0800

    changed title

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ DATA_PATH = PATH.joinpath("data").resolve()
 app = dash.Dash(
     __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}],
 )
-app.title = "Oil & Gas Wells"
+app.title = "Imad's Oil Production Dashboard"
 server = app.server
 
 # Create controls
